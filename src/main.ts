2e46a8fa22a10a29005b49b4498da049decbe351
nectariosss <nektariosg98@hotmail.com>
2023-12-05 22:31:18 +0200

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,16 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+
+
+  const config = new DocumentBuilder()
+  .setTitle('Humandex API')
+  .setDescription('The Humandex API description')
+  .setVersion('1.0')
+  .addTag('humandex')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
 
 
   await app.listen(3000);
